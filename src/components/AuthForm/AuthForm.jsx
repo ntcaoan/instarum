853aacc7a7389cebfe9c979b710c3de9a2b5efd1
Nguyen <nguyen8907@saskpolytech.ca>
2024-08-30 13:02:58 -0600

    some update in the form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Box, Button, Flex, Image, Input, VStack} from "@chakra-ui/react";
 
 const AuthForm = () => {
+    const [isLogin, setIsLogin] = useState(true)
+
     return (
-        <div>AuthForm</div>
+        <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+            <VStack spacing={4}>
+                <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
+                <Input
+                    placeholder='Email'
+                    fontSize={14}
+                    type='email'
+                />
+                <Input
+                    placeholder='Password'
+                    fontSize={14}
+                    type='password'
+                />
+
+                {!isLogin ? <Input placeholder='Confirm password :3' fontSize={14} type='password' /> : null }
+
+                <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}>
+                    {isLogin? "Log in" : "Sign up"}
+                </Button>
+
+                {/*--------- OR --------*/}
+                <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+                    <Box flex={2} h={"1px"} bg={"gray.400"} />
+                    <Text mx={1} color={"white"}>OR</Text>
+                    <Box flex={2} h={"1px"} bg={"gray.400"} />
+                </Flex>
+
+            </VStack>
+        </Box>
     )
 }
 
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,20 +6,23 @@ const AuthPage = () => {
     return (
         <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
             <Container maxW={"container.md"} padding={0}>
-                {/*left hand-side */}
-                <Box display={{base:"none", md:"block"}}>
-                    <Image src={"/auth.png"} h={650} alt={'Phone img'} />
-                </Box>
+                <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+                    {/*left hand-side */}
+                    <Box display={{base:"none", md:"block"}}>
+                        <Image src={"/auth.png"} h={650} alt={'Phone img'} />
+                    </Box>
 
-                {/* right hand-side */}
-                <VStack spacing={4} align={"stretch"}>
-                    <AuthForm />
-                    <Box textAlign={"center"}>Get the app.</Box>
-                    <Flex gap={5} justifyContent={"center"}>
-                        <Image src='/playstore.png' h={"10"} alt='Playstore logo' />
-                        <Image src='/microsoft.png' h={"10"} alt='Microsoft logo' />
-                    </Flex>
-                </VStack>
+                    {/* right hand-side */}
+                    <VStack spacing={4} align={"stretch"}>
+                        <AuthForm />
+                        <Box textAlign={"center"}>Get the app.</Box>
+                        <Flex gap={5} justifyContent={"center"}>
+                            <Image src='/playstore.png' h={"10"} alt='Playstore logo' />
+                            <Image src='/microsoft.png' h={"10"} alt='Microsoft logo' />
+                        </Flex>
+                    </VStack>
+
+                </Flex>
             </Container>
         </Flex>
     )
