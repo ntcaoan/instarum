a523a2c671f345ba55d8432ea51d452bc3c5a11e
Nguyen Tran Cao An <ntcaoann@gmail.com>
2024-09-01 11:14:27 -0600

    add some feartures for auth form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,39 +1,58 @@
 import React, {useState} from 'react'
-import {Box, Button, Flex, Image, Input, VStack} from "@chakra-ui/react";
+import {Box, Button, Flex, Image, Input, VStack, Text} from "@chakra-ui/react";
 
 const AuthForm = () => {
     const [isLogin, setIsLogin] = useState(true)
 
     return (
-        <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-            <VStack spacing={4}>
-                <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
-                <Input
-                    placeholder='Email'
-                    fontSize={14}
-                    type='email'
-                />
-                <Input
-                    placeholder='Password'
-                    fontSize={14}
-                    type='password'
-                />
+        <>
+            <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+                <VStack spacing={4}>
+                    <Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
+                    <Input
+                        placeholder='Email'
+                        fontSize={14}
+                        type='email'
+                    />
+                    <Input
+                        placeholder='Password'
+                        fontSize={14}
+                        type='password'
+                    />
 
-                {!isLogin ? <Input placeholder='Confirm password :3' fontSize={14} type='password' /> : null }
+                    {!isLogin ? <Input placeholder='Confirm password :3' fontSize={14} type='password' /> : null }
 
-                <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}>
-                    {isLogin? "Log in" : "Sign up"}
-                </Button>
+                    <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}>
+                        {isLogin? "Log in" : "Sign up"}
+                    </Button>
 
-                {/*--------- OR --------*/}
-                <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
-                    <Box flex={2} h={"1px"} bg={"gray.400"} />
-                    <Text mx={1} color={"white"}>OR</Text>
-                    <Box flex={2} h={"1px"} bg={"gray.400"} />
+                    {/*--------- OR --------*/}
+                    <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+                        <Box flex={2} h={"1px"} bg={"gray.400"} />
+                        <Text mx={1} color={"white"}>OR</Text>
+                        <Box flex={2} h={"1px"} bg={"gray.400"} />
+                    </Flex>
+
+                    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+                        <Image src='/google.png' w={5} alt='Google Logo' />
+                        <Text mx='2' color={"blue.500"}>
+                            Log in with Google
+                        </Text>
+                    </Flex>
+                </VStack>
+            </Box>
+
+            <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                    <Box mx={2} fontSize={14}>
+                        {isLogin ? "no account ???" : "ooo you have an account??"}
+                    </Box>
+                    <Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
+                        {isLogin ? "sign up" : "log in"}
+                    </Box>
                 </Flex>
-
-            </VStack>
-        </Box>
+            </Box>
+        </>
     )
 }
 
