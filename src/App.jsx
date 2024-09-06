b5b4bcbfee09c54bc097a65252e03eecfcb530b6
Nguyen <nguyen8907@saskpolytech.ca>
2024-09-06 11:36:36 -0600

    starting with homepage and mostly with layout of sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import {Button} from "@chakra-ui/react";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import AuthPage from "./pages/AuthPage/AuthPage.jsx";
+import PageLayout from "./Layouts/PageLayout/PageLayout.jsx";
 
 function App() {
   return (
-    <>
+    <PageLayout>
         <Routes>
             <Route path='/' element={<HomePage />}/>
             <Route path='/auth' element={<AuthPage />}/>
         </Routes>
-    </>
+    </PageLayout>
   )
 }
 
